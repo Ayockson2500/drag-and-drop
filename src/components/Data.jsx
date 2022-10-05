85aa5ea5ed71cfefb3efeb3605a7a248655c8baf
ayockson2500 <ayockson2500@gmail.com>
2022-10-05 12:15:33 +0100

    'updated draganddrop'

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,5 +1,6 @@
 export const data = [
     { title: "Group One", items: ["item 1", "item 2", "item 3", "item 4"] },
     { title: "Group Two", items: ["item 5", "item 6", "item 7"] },
+    { title: "Group Three", items: ["item 8"] },
   ];
   
